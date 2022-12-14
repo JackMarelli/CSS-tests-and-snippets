44165779059bfc4e997ac6a6ed4ac0cbaf23f41a
JackMarelli <marelligiacomo@gmail.com>
2022-12-14 11:27:07 +0100

    timer funziona con ore

--- 20211029_timer/script.js
+++ 20211029_timer/script.js
@@ -1,38 +1,25 @@
 function startTimer(duration) {
-    let display = document.getElementById("timer");
-    if (display.textContent != "00:00") {
-        alert("timer reset");
-        let timer = duration, minutes, seconds;
-        clearInverval(refresh);
+  let display = document.getElementById("timer");
+  let timer = duration;
+  if (display.textContent != "00:00:00") {
+    timer = duration;
+    alert("timer reset to " + duration + " - " + timer);
+    clearInverval(refresh);
+  }
+  let hours, minutes, seconds;
+  let refresh = setInterval(function () {
+    hours = parseInt(timer / 3600, 10);
+    minutes = parseInt((timer % 3600) / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = hours + ":" + minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      display.textContent = "00" + ":" + "00" + ":" + "00";
     }
-    let timer = duration, minutes, seconds;
-    let refresh = setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            display.textContent = "00" + ":" + "00";
-            
-        }
-    }, 1000);
-}
-
-function startMyTimer(durata) {
-    let minuti = durata / 60;
-    let secondi = durata % 60;
-    let parag = document.getElementById("timer");
-    setInterval(() => {
-        if(secondi =="00") {
-            if (--minuti == "00") {
-                break;
-            }
-            minuti--;
-        }
-    }, 1000);
+  }, 1000);
 }
