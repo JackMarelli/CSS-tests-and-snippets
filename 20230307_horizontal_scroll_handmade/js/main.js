61ca3f114d8a7e9d09974dacbf32e0f2c02f5d2f
JackMarelli <marelligiacomo@gmail.com>
2023-03-08 14:26:09 +0100

    horizontal scroll is now smooth as a baby's ass

--- 20230307_horizontal_scroll_handmade/js/main.js
+++ 20230307_horizontal_scroll_handmade/js/main.js
@@ -1,5 +1,34 @@
 const scroller = document.querySelector("#scroller");
+let sx = 0;
+let dx = sx;
+
 window.addEventListener("wheel", (e) => {
-    console.log('e.deltaY', e.deltaY);
-    scroller.scrollLeft += e.deltaY;
+  sx += e.deltaY;
+  if (sx > scroller.scrollWidth) {
+    sx = scroller.scrollWidth;
+  } else if (sx <= 0) {
+    sx = 0;
+  }
+  console.log(sx);
 });
+
+window.requestAnimationFrame(render);
+function render() {
+  //We calculate our container position by linear interpolation method
+  dx = li(dx, sx, 0.05);
+  dx = Math.floor(dx * 100) / 100;
+  
+  if (dx > scroller.scrollWidth) {
+    dx = scroller.scrollWidth;
+  } else if (dx <= 0) {
+    dx = 0;
+  }
+  
+  console.log(dx)
+  scroller.scrollLeft = dx;
+  window.requestAnimationFrame(render);
+}
+
+function li(a, b, n) {
+  return (1 - n) * a + n * b;
+}
