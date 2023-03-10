f70823f65937dff643b0bcede72f386a0700f947
JackMarelli <marelligiacomo@gmail.com>
2023-03-10 18:27:54 +0100

    pointer events but solved, added contact cta

--- 20230307_horizontal_scroll_handmade/js/main.js
+++ 20230307_horizontal_scroll_handmade/js/main.js
@@ -2,36 +2,28 @@ const scroller = document.querySelector("#scroller");
 const cursorDiv = document.querySelector("#cursorDiv");
 const sampleScrollerElement = document.querySelector("#sampleScrollerElement");
 const scrollerElements = document.getElementsByClassName("img-container");
-console.log(scrollerElements);
 let sx = 0;
 let dx = sx;
 
-
 window.addEventListener("wheel", (e) => {
-  sx += e.deltaY;
   if (sx > scroller.scrollWidth) {
     sx = scroller.scrollWidth;
-  } else if (sx <= 0) {
+  } else if (sx < 0) {
     sx = 0;
+  } else {
+    sx += e.deltaY;
   }
 });
 
 window.requestAnimationFrame(render);
 function render() {
-  //We calculate our container position by linear interpolation method
   dx = li(dx, sx, 0.04);
   dx = Math.floor(dx * 100) / 100;
 
-  if (dx > scroller.scrollWidth - sampleScrollerElement.offsetWidth) {
-    dx = scroller.scrollWidth - sampleScrollerElement.offsetWidth;
-  } else if (dx <= 0) {
-    dx = 0;
-  }
+  //console.log(dx);
+  //onsole.log(scroller.scrollWidth);
 
-  //console.log(dx)
   scroller.scrollLeft = dx;
-  //console.log(scroller.scrollWidth);
-
   window.requestAnimationFrame(render);
 }
 
@@ -44,11 +36,10 @@ window.onload = () => {
   cursorDiv.classList.add("cursorDefault");
 }
 
-/*
 window.addEventListener("mousemove", (e) => {
   cursorDiv.style.top = `${e.clientY - cursorDiv.offsetWidth / 2}`;
   cursorDiv.style.left = `${e.clientX - cursorDiv.offsetHeight / 2}`;
-});*/
+});
 
 for (let i = 0; i < scrollerElements.length; i++) {
   console.log("event listeners added")
